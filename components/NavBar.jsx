e49ee93cd16d630bdee4b0550c3582a100748f35
Ashutosh <ashutoshindian2020@gmail.com>
2026-09-24 10:21:44 +0530

    add responsive navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -17,11 +17,11 @@ const NavBar = () => {
     }
 
   return (
-    <nav>
-        <Link href="/products">Admin Dashboard</Link>
-        <div>
-            {user && <span>Hi, {user.username}</span> }
-            <button onClick={handleLogout}>
+    <nav className="flex justify-between px-10 py-4 bg-green-300">
+        <Link href="/products" className=" text-gray-700 sm:px-6 sm:text-2xl font-bold tracking-wide">Admin Dashboard</Link>
+        <div className="flex items-center text-gray-600 gap-5 font-semibold tracking-wider">
+            {user && <span className=" hidden sm:inline">Hi, {user.username}</span> }
+            <button onClick={handleLogout} className="border-gray-400 border px-3 py-2 rounded hover:bg-red-400 outline-0 hover:text-white">
                 Logout
             </button>
         </div>
